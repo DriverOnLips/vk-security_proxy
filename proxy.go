e0b8599e0447f307db13f8839105b0862d5d365a
Romanlock <92111647+DriverOnLips@users.noreply.github.com>
2024-02-22 11:46:46 +0300

    add basic processing

--- proxy.go
+++ proxy.go
@@ -3,117 +3,50 @@ package main
 import (
 	"fmt"
 	"io"
+	"net"
 	"net/http"
+	"strings"
 )
 
-func printRequest(r *http.Request) {
-	fmt.Println("=========Request=========")
-	fmt.Println(r.Method, r.RequestURI, r.Proto)
-	fmt.Println("Host:", r.Host)
-	fmt.Println("User-Agent:", r.UserAgent())
-	fmt.Println("Accept:", r.Header.Get("Accept"))
-	fmt.Println("Proxy-Connection:", r.Header.Get("Proxy-Connection"))
-	fmt.Println()
-}
-
-func printResponse(response *http.Response) {
-	fmt.Println("=========Response=========")
-	fmt.Println(response.Proto, response.Status)
-	fmt.Println("Server: ", response.Header.Get("Server"))
-	fmt.Println("Date: ", response.Header.Get("Date"))
-	fmt.Println("Content-Type: ", response.Header.Get("Content-Type"))
-	fmt.Println("Content-Length: ", response.Header.Get("Content-Length"))
-	fmt.Println("Connection: ", response.Header.Get("Connection"))
-	fmt.Println("Location: ", response.Header.Get("Location"))
-	fmt.Println()
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("Error reading response body")
-	} else {
-		fmt.Println(string(body))
-		fmt.Println()
-	}
-}
-
-func copyRequestHeaders(from *http.Request, to *http.Request) {
-	for name, values := range from.Header {
-		for _, value := range values {
-			to.Header.Set(name, value)
-		}
-	}
-}
-
-func copyResponseHeaders(from *http.Response, to http.ResponseWriter) {
-	for name, values := range from.Header {
-		for _, value := range values {
-			to.Header().Set(name, value)
-		}
-	}
-
-	// set the status code from proxy response to src response
-	to.WriteHeader(from.StatusCode)
-}
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	// printing request
-	printRequest(r)
-
-	// deleting header Proxy-Connection
-	r.Header.Del("Proxy-Connection")
-
-	// replacing path with relative
-	r.RequestURI = r.URL.Path
-
-	// creating proxy request with the same method, url and body
-	proxyReq, err := http.NewRequest(r.Method, r.URL.String(), r.Body)
-	if err != nil {
-		http.Error(w, "cant create proxy request", http.StatusInternalServerError)
+func handleConnect(w http.ResponseWriter, r *http.Request) {
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
 		return
 	}
 
-	// copying headers from src request to proxy request
-	copyRequestHeaders(r, proxyReq)
-
-	// sending proxy request using transport
-	response, err := http.DefaultTransport.RoundTrip(proxyReq)
+	clientConn, _, err := hijacker.Hijack()
 	if err != nil {
-		http.Error(w, "Error sending proxy request", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
 		return
 	}
-	defer response.Body.Close()
+	defer clientConn.Close()
 
-	// printing response
-	printResponse(response)
+	hostPort := r.URL.Host
+	host := strings.Split(hostPort, ":")[0]
+	port := strings.Split(hostPort, ":")[1]
 
-	// copying headers from proxy response to src response
-	copyResponseHeaders(response, w)
+	fmt.Fprintf(clientConn, "HTTP/1.1  200 Connection established\r\n\r\n")
 
-	// reading the body of the proxy response
-	body, err := io.ReadAll(response.Body)
+	serverConn, err := net.Dial("tcp", host+":"+port)
 	if err != nil {
-		http.Error(w, "Error reading proxy response body", http.StatusInternalServerError)
+		fmt.Println(err)
 		return
 	}
+	defer serverConn.Close()
 
-	// writing the body to the main response
-	w.Write(body)
+	go func() {
+		io.Copy(serverConn, clientConn)
+	}()
+	io.Copy(clientConn, serverConn)
 }
-
 
 func main() {
-	// creating server
-	server := http.Server{
-		Addr:    ":8080",
-		Handler: http.HandlerFunc(handleRequest),
-	}
+	http.HandleFunc("/", handleConnect)
 
-	fmt.Println("server started on :8080")
-
-	// starting server
-	err := server.ListenAndServe()
+	fmt.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Errorf("Error starting proxy server: ", err)
+		fmt.Printf("Error starting proxy server: %v\n", err)
 	}
-	
 }
